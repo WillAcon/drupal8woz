acee13b09688cd9aa642c56a9149eb27be21619f
WillAcon <willacon@WillAcons-MacBook-Pro.local>
2020-02-08 23:00:29 -0500

    changes

--- set-theme-name.sh
+++ set-theme-name.sh
@@ -9,9 +9,9 @@ sed -i "s/DRUPAL8WOZ/$1/g" config/schema/DRUPAL8WOZ.schema.yml
 sed -i "s/DRUPAL8WOZ/$1/g" DRUPAL8WOZ.info.yml
 
 mv DRUPAL8WOZ.libraries.yml $1.libraries.yml
+mv DRUPAL8WOZ.info.yml $1.info.yml
 mv DRUPAL8WOZ.theme $1.theme
 
-mv config/install/DRUPAL8WOZ.settings.yml config/install/$1.settings.yml
 mv config/schema/DRUPAL8WOZ.schema.yml config/schema/$1.schema.yml
 
 rm $0
